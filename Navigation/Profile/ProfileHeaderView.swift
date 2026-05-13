08ea1c6c5d66b65225d419517f2cd72b4ab462fb
coldynee <coldynee@gmail.com>
2026-05-13 08:22:09 +0300

    add avatar animation

--- Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -11,7 +11,12 @@ class ProfileHeaderView: UIView {
 
     private var statusText: String = ""
     
-    private lazy var avatarImageView: UIImageView = {
+    private var avatarOriginalFrame = CGRect.zero
+    private var avatarBackground: UIView?
+    private var returnAvatarButton: UIButton?
+    private var avatarCopy: UIImageView?
+    
+    lazy var avatarImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.image = UIImage(named: "avatar")
         imageView.contentMode = .scaleAspectFill
@@ -19,6 +24,10 @@ class ProfileHeaderView: UIView {
         imageView.layer.borderColor = UIColor.white.cgColor
         imageView.layer.borderWidth = 3
         imageView.clipsToBounds = true
+        imageView.isUserInteractionEnabled = true
+        
+        let tapAvatar = UITapGestureRecognizer(target: self, action: #selector(avatarTaped))
+        imageView.addGestureRecognizer(tapAvatar)
         
         return imageView
     }()
@@ -150,5 +159,76 @@ class ProfileHeaderView: UIView {
     @objc func statusTextFieldChanged(_ textField: UITextField) {
         statusText = textField.text ?? ""
         setStatusButton.setTitle("Set status", for: .normal)
+    }
+    
+    @objc func avatarTaped(_ gesture: UITapGestureRecognizer) {
+        guard let window = self.window else { return }
+        
+        avatarImageView.isUserInteractionEnabled = false
+        
+        avatarOriginalFrame = avatarImageView.convert(avatarImageView.bounds, to: window)
+        
+        avatarCopy = UIImageView(image: avatarImageView.image)
+        avatarCopy?.frame = avatarOriginalFrame
+        avatarCopy?.contentMode = .scaleAspectFill
+        avatarCopy?.clipsToBounds = true
+        avatarCopy?.layer.borderColor = UIColor.white.cgColor
+        avatarCopy?.layer.borderWidth = 3
+        avatarCopy?.backgroundColor = .white
+        avatarCopy?.layer.cornerRadius = avatarOriginalFrame.width / 2
+        
+        avatarImageView.isHidden = true
+        
+        avatarBackground = UIView(frame: window.bounds)
+        avatarBackground?.backgroundColor = .black
+        avatarBackground?.alpha = 0
+        window.addSubview(avatarBackground!)
+        
+        window.addSubview(avatarCopy!)
+        
+        returnAvatarButton = UIButton(type: .system)
+        returnAvatarButton?.setImage(UIImage(systemName: "xmark.circle.fill"), for: .normal)
+        returnAvatarButton?.tintColor = .white
+        returnAvatarButton?.alpha = 0
+        returnAvatarButton?.addTarget(self, action: #selector(returnAvatarToOrigin), for: .touchUpInside)
+        returnAvatarButton?.frame = CGRect(x: window.bounds.width - 60, y: 60, width: 44, height: 44)
+        window.addSubview(returnAvatarButton!)
+        
+        let targetSize = window.bounds.width - 32
+        UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseInOut) {
+            self.avatarBackground?.alpha = 0.7
+            self.avatarCopy?.center = window.center
+            self.avatarCopy?.bounds = CGRect(x: 0, y: 0, width: targetSize, height: targetSize)
+            self.avatarCopy?.layer.cornerRadius = 0
+        }
+        
+        UIView.animate(withDuration: 0.3, delay: 0.5, options: .curveEaseInOut) {
+            self.returnAvatarButton?.alpha = 1
+        }
+    }
+    
+    @objc private func returnAvatarToOrigin() {
+        guard let background = avatarBackground,
+              let copy = avatarCopy,
+              let button = returnAvatarButton else { return }
+        
+        UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseInOut) {
+            button.alpha = 0
+            copy.frame = self.avatarOriginalFrame
+            copy.layer.cornerRadius = self.avatarOriginalFrame.width / 2
+            background.alpha = 0
+        } completion: { _ in
+            self.avatarImageView.isHidden = false
+            
+            copy.removeFromSuperview()
+            background.removeFromSuperview()
+            button.removeFromSuperview()
+            
+            self.avatarBackground = nil
+            self.avatarCopy = nil
+            self.returnAvatarButton = nil
+            
+            self.avatarImageView.isUserInteractionEnabled = true
+        }
     }
 }
